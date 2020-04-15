6e942d773deb49bd57fb1adcb926bb91c6452e1a
Vitor Krasniqi <vitor.krasniqi@stud.unilu.ch>
2020-04-15 18:28:19 +0200

    second commit

--- src/02_Creating_the_panel_dataset.R
+++ src/02_Creating_the_panel_dataset.R
@@ -1,1 +1,22 @@
 getwd()
+
+
+
+# Trying to understand the following stata data
+
+# clonevar wave = cs08a_m
+# foreach y of numlist 1/374 379/407 { 
+#   local Y : di %03.0f `y' 
+#      clonevar csvar`Y' = cs08a`Y' 
+# }
+# drop cs08*
+# 
+# append using "4 Social Integration and Leisure\cs09b_1p_EN.dta", gen(panel)
+# replace year = 2009 if panel == 1
+# replace wave = cs09b_m if year == 2009
+# drop panel
+# foreach y of numlist 1/374 379/407 { 
+#      local Y : di %03.0f `y' 
+# replace csvar`Y' = cs09b`Y' if year == 2009
+# }
+# drop cs09* 
