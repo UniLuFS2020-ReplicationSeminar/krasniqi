5c9ae61fc620b49bd06f6adb4c8d129e9a3eab65
Vitor Krasniqi <vitor.krasniqi@stud.unilu.ch>
2020-04-29 04:17:02 +0200

    Creating the Datasets

--- src/02_Creating_the_panel_dataset.R
+++ src/02_Creating_the_panel_dataset.R
@@ -20,3 +20,40 @@ getwd()
 # replace csvar`Y' = cs09b`Y' if year == 2009
 # }
 # drop cs09* 
+
+
+# Columns chosen from Stata 1/374 379/407
+
+
+cs08a_2p_EN <- cs08a_2p_EN[,c(1:374, 379:407)]
+
+cs08a_2p_EN$year = '2008'
+
+
+cs09b_1p_EN <- cs09b_1p_EN[,c(1:374, 379:407)]
+
+cs09b_1p_EN$year = '2009'
+
+cs10c_1p_EN <- cs10c_1p_EN[,c(1:374, 379:407)]
+cs10c_1p_EN$year = '2010'
+
+cs11d_EN_3_0p  <- cs11d_EN_3_0p[,c(1:374, 379:407)]
+
+
+cs11d_EN_3_0p$year = '2011'
+
+
+
+
+cs12e_1_0p_EN$year = '2012'
+
+cs13f_2_0p_ENyear = '2013'
+
+cs14g_EN_2_0$year = '2014'
+
+cs15h_EN_1_0p$year = '2015'
+
+cs16i_EN_1_0p$year = '2016'
+
+cs17j_EN_1_0p$year = '2017'
+
